f9949778f5acc074095d5d4b9b1813ae143da5ac
Armno <monkeyarmno@gmail.com>
2015-07-01 15:55:31 +0700

    add animations properly

--- app/src/main/java/bhouse/travellist_starterproject/MainActivity.java
+++ app/src/main/java/bhouse/travellist_starterproject/MainActivity.java
@@ -3,13 +3,19 @@ package bhouse.travellist_starterproject;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.ActivityOptionsCompat;
+import android.support.v4.util.Pair;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Toast;
+import android.view.Window;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toolbar;
 
 
 public class MainActivity extends Activity {
@@ -19,6 +25,8 @@ public class MainActivity extends Activity {
 
     private RecyclerView mRecyclerView;
     private StaggeredGridLayoutManager mStaggeredLayoutManager;
+
+  private Toolbar toolbar;
 
   private TravelListAdapter mAdapter;
 
@@ -39,19 +47,75 @@ public class MainActivity extends Activity {
     TravelListAdapter.OnItemClickListener onItemClickListener = new TravelListAdapter.OnItemClickListener() {
       @Override
       public void onItemClick(View view, int position) {
-        Toast.makeText(MainActivity.this, "Clicked " + position, Toast.LENGTH_SHORT).show();
+//        Toast.makeText(MainActivity.this, "Clicked " + position, Toast.LENGTH_SHORT).show();
+//
+//        Intent intent = new Intent(MainActivity.this, DetailActivity.class);
+//        intent.putExtra(DetailActivity.EXTRA_PARAM_ID, position);
+//        startActivity(intent);
+//
+//        ImageView placeImage = (ImageView) view.findViewById(R.id.placeImage);
+//        LinearLayout placeNameHolder = (LinearLayout) view.findViewById(R.id.placeNameHolder);
+//
+//        Pair<View, String> imagePair = Pair.create((View) placeImage, "tImage");
+//        Pair<View, String> holderPair = Pair.create((View) placeNameHolder, "tNameHolder");
+//
+//        ActivityOptionsCompat options = ActivityOptionsCompat.makeSceneTransitionAnimation(MainActivity.this, imagePair, holderPair);
+//        ActivityCompat.startActivity(MainActivity.this, intent, options.toBundle());
 
-        Intent intent = new Intent(MainActivity.this, DetailActivity.class);
-        intent.putExtra(DetailActivity.EXTRA_PARAM_ID, position);
-        startActivity(intent);
+//        Intent transitionIntent = new Intent(MainActivity.this, DetailActivity.class);
+//        transitionIntent.putExtra(DetailActivity.EXTRA_PARAM_ID, position);
+//        ImageView placeImage = (ImageView) view.findViewById(R.id.placeImage);
+//        LinearLayout placeNameHolder = (LinearLayout) view.findViewById(R.id.placeNameHolder);
+//
+//        View navigationBar = findViewById(android.R.id.navigationBarBackground);
+//        View statusBar = findViewById(android.R.id.statusBarBackground);
+//
+//        Pair<View, String> imagePair = Pair.create((View) placeImage, "tImage");
+//        Pair<View, String> holderPair = Pair.create((View) placeNameHolder, "tNameHolder");
+//
+//        Pair<View, String> navPair = Pair.create(navigationBar, Window.NAVIGATION_BAR_BACKGROUND_TRANSITION_NAME);
+//        Pair<View, String> statusPair = Pair.create(statusBar, Window.STATUS_BAR_BACKGROUND_TRANSITION_NAME);
+//        Pair<View, String> toolbarPair = Pair.create((View) toolbar, "tActionBar");
+//
+//        ActivityOptionsCompat options = ActivityOptionsCompat.makeSceneTransitionAnimation(MainActivity.this, imagePair, holderPair, navPair, statusPair, toolbarPair);
+//        ActivityCompat.startActivity(MainActivity.this, transitionIntent, options.toBundle());
+
+        // 1
+        Intent transitionIntent = new Intent(MainActivity.this, DetailActivity.class);
+        transitionIntent.putExtra(DetailActivity.EXTRA_PARAM_ID, position);
+        ImageView placeImage = (ImageView) view.findViewById(R.id.placeImage);
+        LinearLayout placeNameHolder = (LinearLayout) view.findViewById(R.id.placeNameHolder);
+        // 2
+        View navigationBar = findViewById(android.R.id.navigationBarBackground);
+        View statusBar = findViewById(android.R.id.statusBarBackground);
+
+        Pair<View, String> imagePair = Pair.create((View) placeImage, "tImage");
+        Pair<View, String> holderPair = Pair.create((View) placeNameHolder, "tNameHolder");
+        // 3
+        Pair<View, String> navPair = Pair.create(navigationBar,
+                Window.NAVIGATION_BAR_BACKGROUND_TRANSITION_NAME);
+        Pair<View, String> statusPair = Pair.create(statusBar, Window.STATUS_BAR_BACKGROUND_TRANSITION_NAME);
+        Pair<View, String> toolbarPair = Pair.create((View)toolbar, "tActionBar");
+        // 4
+        ActivityOptionsCompat options = ActivityOptionsCompat.makeSceneTransitionAnimation(MainActivity.this,
+                imagePair, holderPair, navPair, statusPair, toolbarPair);
+        ActivityCompat.startActivity(MainActivity.this, transitionIntent, options.toBundle());
       }
     };
 
     mAdapter.setOnItemClickListener(onItemClickListener);
+
+    toolbar = (Toolbar) findViewById(R.id.toolbar);
+    setUpActionBar();
   }
 
   private void setUpActionBar() {
-
+    if (toolbar != null) {
+      setActionBar(toolbar);
+      getActionBar().setDisplayHomeAsUpEnabled(false);
+      getActionBar().setDisplayShowTitleEnabled(true);
+      getActionBar().setElevation(7);
+    }
   }
 
   @Override
